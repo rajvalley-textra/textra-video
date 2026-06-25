4cf24f578fe6667a1dba1cc9689ff9849b97fd02
Raj Valley <rajvalley@gmail.com>
2026-06-25 12:22:21 +0100

    Increase Raj, Steven, Stuart image scale to 1.4 to make them larger

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -25,7 +25,7 @@ export default function AboutPage() {
       linkedin: 'https://www.linkedin.com/in/rajvalley/',
       image: '/assets/Profile pics/Raj.png',
       imagePos: 'center 36%',
-      imageScale: 1.1,
+      imageScale: 1.4,
     },
     {
       name: 'Steven Godman',
@@ -34,7 +34,7 @@ export default function AboutPage() {
       linkedin: 'https://www.linkedin.com/in/stevegodman/',
       image: '/assets/Profile pics/Steve.png',
       imagePos: 'center 36%',
-      imageScale: 1.1,
+      imageScale: 1.4,
     },
     {
       name: 'Stuart Jeffrey',
@@ -43,7 +43,7 @@ export default function AboutPage() {
       linkedin: 'https://www.linkedin.com/in/stuart-jeffrey-9a3739/',
       image: '/assets/Profile pics/Stuart.png',
       imagePos: 'center 38%',
-      imageScale: 1.1,
+      imageScale: 1.4,
     },
     {
       name: 'Alexis Cheshire',
